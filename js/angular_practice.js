7f097638c22c791c8fe0067a5ce3698d8072dae0
jay nichols <jay_nichols_3@hotmail.com>
2017-01-18 18:16:38 -0600

    added controller, modified html

--- js/angular_practice.js
+++ js/angular_practice.js
@@ -1,5 +1,12 @@
 "use strict";
 
 (function() {
-    var app = angular.module("moduleName", []);
+    var app = angular.module("myApp", []);
+    app.controller('appController', function($scope) {
+    	$scope.firstName = "Jay";
+    	$scope.lastName = "Nichols";
+    	$scope.fullName = function() {
+    		return $scope.firstName + " " + $scope.lastName;
+    	};
+    });
 })();
