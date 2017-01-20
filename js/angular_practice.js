9a43f7ccc6e5cda71b694a207465fe59808cb74e
jay nichols <jay_nichols_3@hotmail.com>
2017-01-20 13:01:34 -0600

    minor changes

--- js/angular_practice.js
+++ js/angular_practice.js
@@ -5,6 +5,7 @@
     app.controller('appController', function($scope, $location) {
     	$scope.firstName = "Jay";
     	$scope.lastName = "Nichols";
+    	$scope.count = 0;
     	$scope.thisUrl = $location.absUrl();
     	$scope.fullName = function() {
     		return $scope.firstName + " " + $scope.lastName;
